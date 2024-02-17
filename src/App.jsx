40766f8c54c240b3eb28edb7ea5cd3d1dc61631e
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-17 16:31:40 -0500

    Added start button to begin the game

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,33 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const difficulty = useState(false);
+  const [start, setStart] = useState(false);
+  let startGame;
+  if (start) {
+    startGame = (
+      <>
+        <button>Easy</button>
+        <button>Medium</button>
+        <button>Hard</button>
+        <Cards />
+      </>
+    );
+  }
 
   return (
     <>
-      <Cards />
+      {!start && (
+        <div>
+          <div>
+            This is the DBZ Memory Card Game. Let's see how much are you a true
+            fan of the legendary series by putting your memory skills to the
+            test
+          </div>
+          <button onClick={() => setStart(!start)}>Start Game</button>
+        </div>
+      )}
+      {startGame}
     </>
   );
 }
