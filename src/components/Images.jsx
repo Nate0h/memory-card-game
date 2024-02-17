4d4b90c0250a4f17e1e7698e22dabe90ad963d21
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-02-16 21:45:35 -0500

    Promise.all to fetch images+ return Promise array

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -5,27 +5,35 @@ function Images() {
   /* const apiKey = "sqn6kfwMf3k9Lc9yoWZBsHkh8VHHDQ9E";
   let endpointID = "adssad";*/
   const [imageUrl, setImageUrl] = useState("");
+  const [imageUrlList, setImageUrlList] = useState([]);
   let url = `https://api.giphy.com/v1/gifs/bLy8cwoobMGx1uDHkO?api_key=sqn6kfwMf3k9Lc9yoWZBsHkh8VHHDQ9E&rating=g`;
   const images = [
-    { id: 0, character: "Goku" },
-    { id: 1, character: "Vegeta" },
-    { id: 2, character: "Gohan" },
-    { id: 3, character: "Trunks" },
-    { id: 4, character: "Piccolo" },
-    { id: 5, character: "Krillin" },
+    { id: 0, character: "Goku", endPoint: "bLy8cwoobMGx1uDHkO" },
+    { id: 1, character: "Vegeta", endPoint: "bLy8cwoobMGx1uDHkO" },
+    { id: 2, character: "Gohan", endPoint: "bLy8cwoobMGx1uDHkO" },
+    { id: 3, character: "Trunks", endPoint: "bLy8cwoobMGx1uDHkO" },
+    { id: 4, character: "Piccolo", endPoint: "bLy8cwoobMGx1uDHkO" },
+    { id: 5, character: "Krillin", endPoint: "bLy8cwoobMGx1uDHkO" },
   ];
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(url);
-      const json = await response.json();
-      setImageUrl(json.data.images.original.url);
+      let randomUrls = await Promise.all(
+        images.map(async (image) => {
+          let newUrl = `https://api.giphy.com/v1/gifs/${image.endPoint}?api_key=sqn6kfwMf3k9Lc9yoWZBsHkh8VHHDQ9E&rating=g`;
+          const response = await fetch(newUrl);
+          const json = await response.json();
+          return json.data.images.original.url;
+        })
+      );
+
+      setImageUrlList(randomUrls);
     }
     fetchData();
   }, []);
   return (
     <>
-      <img src={imageUrl} alt="" />
+      <img src={imageUrlList[5]} alt="" />
     </>
   );
 }
